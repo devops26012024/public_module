fadd7ea1d4e96da2a5c1fb5ef78e632a552f88f7
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 14:02:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 module "vpc" {
-   source             = "git::https://github.com/devops26012024/private_module.git?ref=v1.0.0"
+   source = "git::https://github.com/devops26012024/private_module.git?ref=v1.0.0"
 }
