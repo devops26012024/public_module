af8315c20b1397a94b14d2feaaf6e5923e9085e9
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-20 09:06:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
 module "vpc" {
-  source             = "git::github.com/devops26012024/private_module//?ref=v1.0.0"
-  name               = "example"
-  vpc_cidr           = "10.0.0.0/16"
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
-  subnet_cidrs       = ["10.0.1.0/24", "10.0.2.0/24"]
-  is_public          = [true, false]
-  tags = {
-    Environment = "Production"
+  source             = "https://github.com/devops26012024/private_module.git"
+  #name               = "example"
+  #vpc_cidr           = "10.0.0.0/16"
+  #availability_zones = ["ap-south-1a", "ap-south-1b"]
+  #subnet_cidrs       = ["10.0.1.0/24", "10.0.2.0/24"]
+  #is_public          = [true, false]
+  #tags = {
+   # Environment = "Production"
+  #}
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "pradeep-terraform-1234333"
+    key            = "pub-module/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-demo"
+    encrypt        = true
   }
 }
